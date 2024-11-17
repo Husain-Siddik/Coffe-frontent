e2f2da86bd67ad449171b6e53e64c2b5c634a585
Husain-Siddik <ronysiddik21@gmail.com>
2024-11-18 03:05:33 +0600

    iconeic section added some transition effect added and respinsibl

---
+++ src/components/awsomeItem .vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="">
+
+        <div class=" bg-[#ECEAE3]  flex flex-col lg:flex-row gap-5 md:gap-12 items-center justify-center  p-3">
+
+            <div
+                class=" w-auto md:w-80 hover:w-96 border-2  space-y-2 md:space-y-5 p-14 mt-5 hover:bg-[#a59b7a] transition-all ease-out delay-300 ">
+                <img src="../assets/images/icons/1.png" alt="" srcset="">
+                <h1 class="font-MyFont text-4xl">Awesome Aroma</h1>
+                <p class="font-ralway">You will definitely be a fan of the design & aroma of your coffee</p>
+            </div>
+            <div
+                class=" w-auto md:w-80 hover:w-96 border-2  space-y-2 md:space-y-5 p-14 mt-5 hover:bg-[#a59b7a] transition-all ease-out delay-300">
+                <img src="../assets/images/icons/2.png" alt="" srcset="">
+                <h1 class="font-MyFont text-4xl">High Quality</h1>
+                <p class="font-ralway">We served the coffee to you maintaining the best quality</p>
+            </div>
+            <div
+                class="w-auto md:w-80 hover:w-96 border-2  space-y-2 md:space-y-5 p-14 mt-5 hover:bg-[#a59b7a] transition-all ease-out delay-300">
+                <img src="../assets/images/icons/3.png" alt="" srcset="">
+                <h1 class="font-MyFont text-4xl">Pure Grades</h1>
+                <p class="font-ralway">The coffee is made of the green coffee beans which you will love</p>
+            </div>
+            <div
+                class="w-auto md:w-80 hover:w-96 border-2  space-y-2 md:space-y-5 p-14 mt-5 hover:bg-[#a59b7a] transition-all ease-out delay-300">
+                <img src="../assets/images/icons/4.png" alt="" srcset="">
+                <h1 class="font-MyFont text-4xl">Proper Roasting</h1>
+                <p class="font-ralway">Your coffee is brewed by first roasting the green coffee beans</p>
+            </div>
+
+
+
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
 import banerItem from '@/components/banerItem.vue';
+import awsomeItem from '@/components/awsomeItem .vue';
 </script>
 
 <template>
   <main>
     <!-- <TheWelcome /> -->
     <banerItem />
+    <awsomeItem />
   </main>
 </template>
